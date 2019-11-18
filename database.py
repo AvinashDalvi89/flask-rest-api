9b95e1ff4db72f2ff887d8b5a7eb568dec16ecc5
Avinash <avinash.d@knabfinance.com>
2019-11-19 00:02:59 +0530

    added mysql connection library to connect database

---
+++ database.py
@@ -0,0 +1,67 @@
+import mysql.connector
+import time
+from utility import *
+import traceback
+
+
+class DataBase(Utility):
+    """
+    Class which HollogMsgds DataBase Implementations
+    """
+    module_name = "DataBase"
+
+    def __init__(self):
+        self.connection_obj = None
+        Utility.__init__(self)
+        self.create_connection_obj()
+
+    def create_connection_obj(self):
+        try:
+            # print "akhgakhfg"
+            self.connection_obj = mysql.connector.connect(**self.connection_details)
+            self.connection_obj.autocommit = True
+            print "connected to db", self.connection_obj
+        except Exception, ex:
+            print "DB Connection Exception while creating connection", ex
+            self.connection_obj = None
+        except:
+            print "Unknown Error in MySQL connection creation" * 10
+            self.connection_obj = None
+
+    def execute_statement(self, sql_stmt, module="", parameters=(),is_insert=False):
+        out = []
+        st = time.time()
+        split_stmt = sql_stmt.split("\n")
+        sql_stmt_print = " ".join(split_stmt)
+
+        try:
+            pCur = self.connection_obj.cursor()
+            retValue = pCur.execute(sql_stmt, parameters)
+            if is_insert:
+                out.append({"id": pCur.lastrowid})
+            else:
+                if pCur.description:
+                    colnames = map(lambda a: a[0], pCur.description)
+                    out = map(lambda a: dict(zip(colnames, a)), pCur.fetchall())
+                else:
+                    if retValue == -1:
+                        out = []
+            rows_affected = pCur.rowcount
+        except Exception, e:
+            print "Error in query execution %s %s (%s)" % (time.ctime(), sql_stmt_print,parameters)
+            raise Exception(e)
+            out = []
+        split_stmt = sql_stmt.split("\n")
+        sql_stmt_print = " ".join(split_stmt)
+        return out
+
+    def __del__(self):
+        try:
+            print "Cleaning the DB Connection ", self.connection_obj
+            if self.connection_obj:
+                self.connection_obj.close()
+        except Exception, msg:
+            print "Error in closing connection, not able to close", msg
+        except:
+            print "Unknown Error in closing connection, not able to close"
+        print "Done Clean DB connection"
